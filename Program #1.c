a05b9de21133352f7b928b2d54f76854485f07ac
miromoll <miromoll@hotmail.com>
2017-04-24 13:13:37 +0700

    edit Program1

--- Program #1.c
+++ Program #1.c
@@ -2,7 +2,7 @@
 void main()
 {
 
-  char Name[31][40] = {"Ohh Martha", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
+  char Name[30][40] = {"Ohh Martha", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
 
   int ID[30] = {5555, 5302, 5303, 5304, 5305, 5306}; 
   float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26}; 
